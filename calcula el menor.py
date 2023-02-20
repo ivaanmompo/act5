062a4b10fc1d2bf3d836d5577cce80635fc77551
ivan <ivanchova2004@gmail.com>
2023-02-20 09:48:21 +0100

    primer commit

---
+++ calcula el menor.py
@@ -0,0 +1,18 @@
+from os import major
+
+
+n1=float(input("Dime el primer numero: "))
+n2=float(input("Dime el segon numero: "))
+n3=float(input("Dime el tercer numero: "))
+
+major = 0
+
+if n1 > n2 and n1 > n3:
+    major = n1
+else:
+    if n2 > n3:
+        major = n2
+    else:
+        major = n3
+
+print ("El numero major es: " , major)
